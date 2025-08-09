0d64cd79ec8dea2dd5f11bb9469a41e0cf1c9aca
RishanMahajan <rmahajan2_be23@thapar.edu>
2025-08-09 23:03:02 +0530

    ETL Pipeline completed

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -5,7 +5,7 @@ from datetime import datetime
 log_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 log_path=os.path.join(os.getcwd(),"logs",log_file)
-os.makedirs('log_path',exist_ok=True)
+os.makedirs(log_path,exist_ok=True)
 
 log_file_path=os.path.join(log_path,log_file)
 
---
+++ push_data.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import json
+
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_DB_URL=os.getenv('MONGO_DB_URL')
+
+import certifi
+ca=certifi.where()
+
+import pandas as pd
+import numpy as np
+import pymongo
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def csv_to_json(self,file_path):
+        try:
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def insert_data_mongodb(self,records,database,collection):
+        try:
+            self.records=records
+            self.database=database
+            self.collection=collection
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL,tlsCAFile=ca)
+
+            self.database=self.mongo_client[self.database]
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return (len(self.records))
+        
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+
+if __name__=='__main__':
+    file_path=r'Network_Data/phisingData.csv'
+    database='RishanAI'
+    collection='NetworkData'
+    network_obj=NetworkDataExtract()
+    records=network_obj.csv_to_json(file_path=file_path)
+    no_of_records=network_obj.insert_data_mongodb(records,database,collection)
+    print(no_of_records)
