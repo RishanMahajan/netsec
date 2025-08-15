c84fa08c105d65b259c1feded77b927b706b8e45
RishanMahajan <rmahajan2_be23@thapar.edu>
2025-08-16 00:17:45 +0530

    Data Ingestion Component

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -12,5 +12,5 @@ log_file_path=os.path.join(log_path,log_file)
 logging.basicConfig(
     filename=log_file_path,
     level=logging.INFO,
-    format="%(asctime)s-%(lineno)d %(name)s-%(level)s-%(message)s"
+    format="%(asctime)s-%(lineno)d %(name)s-%(levelname)s-%(message)s"
 )
