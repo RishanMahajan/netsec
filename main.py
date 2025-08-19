5347cd96bc73aa91bed9cb8f5922a0607556c801
RishanMahajan <rmahajan2_be23@thapar.edu>
2025-08-20 03:11:28 +0530

    Data Transformation Done

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 
@@ -12,11 +14,21 @@ if __name__=='__main__':
         trainingpipelineconfig=TrainingPipelineConfig()
         dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
         data_ingestion=DataIngestion(dataingestionconfig)
+        logging.info('Data Ingestion Started')
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info('Data Ingestion Completed')
         print(dataingestionartifact)
         data_validation_config=DataValidationConfig(trainingpipelineconfig)
         data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info('Initiated Data Validated')
         data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info('Data Validation Completed')
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info('Initiated Data Transformation')
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info('Data Transformation Done')
+        print(data_transformation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -5,6 +5,7 @@ import pickle
 import yaml
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
+import numpy as np
 
 def read_yaml_file(file_path:str)->dict:
     try:
@@ -27,3 +28,26 @@ def write_yaml_file(file_path:str,content:object,replace:bool=False)->None:
             yaml.dump(content,file)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+    
+
+def save_numpy_array_data(file_path:str,array:np.array):
+    '''
+    Save Numpy array data as file
+    array -> np.array data to save
+    file_path -> str location of file to save
+    '''
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,'wb') as file_obj:
+            np.save(file_obj,array)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
+    
+def save_object(file_path:str,obj:object):
+    try:
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,'wb') as file_obj:
+            pickle.dump(obj,file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
